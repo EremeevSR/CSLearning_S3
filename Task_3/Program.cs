44335afe07084850fc7671d2a3564ce0d5731128
Sergey_415 <sergey123.3415@gmail.com>
2022-05-21 16:38:57 +0500

    Решение задачи 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на
+// вход число (N), а выдает таблицу кубов
+// чисел от 1 до N.
+// Пример:
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+// При решении задачи условимся, что правильность ввода
+// проверять не нужно. 
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+int cubeOfNum(int num)
+{
+    // Метод вычисления куба числа num
+    return num * num * num;
+}
+
+void showCubeTable(int userNumber)
+{
+    // Метод вывода кубов от 1 до userNumber
+    for(int i = 1; i <= userNumber; i++)
+    {
+        if (i == userNumber) //Чтобы вывод был, как в примере
+            Console.WriteLine(cubeOfNum(i));
+        else                 
+            Console.Write(cubeOfNum(i) + ", ");
+    }
+}
+
+showCubeTable(N);
